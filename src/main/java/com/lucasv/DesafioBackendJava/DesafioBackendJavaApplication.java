b9f09a6080b4f2dba48e8cd33829c6eb0461dae3
Lucas Valadao <lukasfv10@gmail.com>
2022-12-12 20:36:50 -0300

    Projeto criado

---
+++ src/main/java/com/lucasv/DesafioBackendJava/DesafioBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasv.DesafioBackendJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasv/DesafioBackendJava/DesafioBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasv.DesafioBackendJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
