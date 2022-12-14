a712dbe4257e41dffb8a3e3d7401bf9b05098df0
Lucas Valadao <lukasfv10@gmail.com>
2022-12-14 18:45:41 -0300

    Implementando o SwaggerAPI e inserindo as Annotations nos controladores TaskResource e UserResource

--- src/main/java/com/lucasv/DesafioBackendJava/resources/TaskResource.java
+++ src/main/java/com/lucasv/DesafioBackendJava/resources/TaskResource.java
@@ -2,6 +2,8 @@ package com.lucasv.DesafioBackendJava.resources;
 
 import com.lucasv.DesafioBackendJava.entities.Task;
 import com.lucasv.DesafioBackendJava.services.TaskService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,18 +13,22 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@Api(tags = "tasks")
 @RequestMapping(value = "/tasks")
 public class TaskResource {
 
     @Autowired
     private TaskService service;
+
     @GetMapping
+    @ApiOperation(value = "${TaskResource.findAll}")
     public ResponseEntity<List<Task>> findAll(){
         List<Task> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @PostMapping
+    @ApiOperation(value = "${TaskResource.insert}")
     public ResponseEntity<Task> insert(@RequestBody Task obj){
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -30,12 +36,14 @@ public class TaskResource {
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiOperation(value = "${TaskResource.delete}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping(value = "/{id}")
+    @ApiOperation(value = "${TaskResource.update}")
     public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task obj){
         obj = service.update(id, obj);
         return ResponseEntity.ok().body(obj);
--- src/main/java/com/lucasv/DesafioBackendJava/resources/UserResource.java
+++ src/main/java/com/lucasv/DesafioBackendJava/resources/UserResource.java
@@ -8,11 +8,17 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(value = "/users")
+@Api(tags = "users")
 @RequiredArgsConstructor
 public class UserResource {
 
@@ -20,24 +26,44 @@ public class UserResource {
     private final UserService service;
 
     @PostMapping("/signin")
-    public String login(@RequestParam String username,@RequestParam String password){
+    @ApiOperation(value = "${UserResource.signin}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Algo deu errado"),
+            @ApiResponse(code = 422, message = "Nome de usuario/senha invalidos")})
+    public String login(@ApiParam("Username") @RequestParam String username,@ApiParam("Password") @RequestParam String password){
         return service.signin(username,password) ;
     }
 
     @PostMapping("/signup")
-    public String signup(@RequestBody UserDataDTO user) {
+    @ApiOperation(value = "${UserResource.signup}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Algo deu errado"),
+            @ApiResponse(code = 403, message = "Acesso negado"),
+            @ApiResponse(code = 422, message = "Nome de usuario ja esta em uso")})
+    public String signup(@ApiParam("Signup User") @RequestBody UserDataDTO user) {
         return service.signup(modelMapper.map(user,User.class));
     }
 
 
     @GetMapping(value = "/{username}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public UserResponseDTO search(@PathVariable String username) {
+    @ApiOperation(value = "${UserResource.search}", response = UserResponseDTO.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Algo deu errado"),
+            @ApiResponse(code = 403, message = "Acesso negado"),
+            @ApiResponse(code = 404, message = "O usuario nao existe"),
+            @ApiResponse(code = 500, message = "JWT Token invalido ou expirado")})
+    public UserResponseDTO search(@ApiParam("Username") @PathVariable String username) {
         return modelMapper.map(service.search(username), UserResponseDTO.class);
     }
 
     @GetMapping(value = "/me")
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_CLIENT')")
+    @ApiOperation(value = "${UserResource.me}", response = UserResponseDTO.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Algo deu errado"),
+            @ApiResponse(code = 403, message = "Acesso negado"),
+            @ApiResponse(code = 500, message = "JWT Token invalido ou expirado")})
     public UserResponseDTO whoami(HttpServletRequest req) {
         return modelMapper.map(service.whoami(req), UserResponseDTO.class);
     }
